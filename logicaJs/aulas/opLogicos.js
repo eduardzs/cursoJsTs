dfc8a4ae43e2f617188d60c8997877f823c5315a
Eduardo Fonseca <eduardofonseca0210@gmail.com>
2022-11-11 17:10:49 -0300

    alterações
    pequenas alterações

---
+++ logicaJs/aulas/opLogicos.js
@@ -0,0 +1,19 @@
+/* 
+    <==Operadores Lógicos==>
+
+    • && -> and -> e
+    • || -> or -> ou
+    • ! -> not -> não
+*/
+
+console.log(10 === 10 && 11 === 11) // Ambas devem retornar true, para que a saída seja true
+console.log(10 === 10 || 10 === 11) // Pelo menos uma deve retornar true, para que a saída seja true
+console.log(!10 === 10) // Inverte o resultado
+
+const usuario = 'Eduardo' // Declara a variável e seu valor
+const senha = '12345678' // Declara a variável e seu valor
+
+// Valida às comparações para que ambas retorne true, e armazena o resultado na variável
+const validarLogin = usuario === 'Eduardo' && senha === '12345678' 
+
+console.log(`Seu Login e Senha São ${validarLogin}! Seja Bem-vindo ${usuario}!`) // Exibe o resultado no console
